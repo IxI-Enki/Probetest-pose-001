8d784fb049ca0aea30a0169c8833f2455a301fa1
Enki <138018029+IxI-Enki@users.noreply.github.com>
2023-11-14 11:42:34 +0100

    Add files via upload

---
+++ hand-in/ErsterProbeTest6.cs
@@ -0,0 +1,23 @@
+﻿namespace ErsterProbetest6
+{
+  class Program
+  {
+    static void Main()
+    {
+      /* VARIABLES: */
+      int n,
+         i = 1;
+      /* INPUT: */
+      Console.Write("\n positive Ganzzahl eingeben:  ");
+      n = Convert.ToInt32(Console.ReadLine());
+      /* CALCULATION: */
+      while (n / 10 != 0)
+      {
+        i++;
+        n = n / 10;
+      }
+      /* OUTPUT: */
+      Console.Write($"\n Die Zahl hatte: {i}Stellen. ");
+    }
+  }
+}
